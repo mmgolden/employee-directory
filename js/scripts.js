ecd4236ab7933bf341203e337f67f2605b6488a1
Melinda Golden <info@melindagolden.com>
2018-11-15 10:13:10 -0500

    Added styling to modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Variables
 const body = document.querySelector('body');
 const gallery = document.getElementById('gallery');
 const searchContainer = document.querySelector('.search-container');
@@ -91,7 +92,7 @@ function showModal(card) {
     // Create the markup to go inside the modal container
     const markup = `
         <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <button type="button" id="modal-close-btn" class="modal-close-btn" aria-label="Close"><i class="fa fa-times" aria-hidden="true"></i></button>
             <div class="modal-info-container">
                 <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
@@ -104,8 +105,8 @@ function showModal(card) {
             </div>
         </div>
         <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            <button type="button" id="modal-prev" class="modal-prev btn" aria-label="Previous"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+            <button type="button" id="modal-next" class="modal-next btn" aria-label="Next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
         </div>
     `;
     modalContainer.innerHTML = markup;
@@ -132,6 +133,7 @@ function addSearch() {
     searchContainer.innerHTML = markup;
 }
 
+// Search employees on the page
 function search(event) {
 
     // Prevent the page from refreshing
@@ -179,11 +181,18 @@ function search(event) {
 // Toggle between the previous and next employee
 function toggleEmployee(event) {
 
+    const target = event.target;
+
     // Get all of the employees from the page
     const employeeList = Array.from(document.querySelectorAll('.card'));
 
     // Get the current modal
-    const modal = event.target.parentElement.previousElementSibling;
+    let modal;
+    if (target.id === 'modal-prev' || target.id === 'modal-next') {
+        modal = target.parentElement.previousElementSibling;
+    } else if (target.className === 'fa fa-chevron-left' || target.className === 'fa fa-chevron-right') {
+        modal = target.parentElement.parentElement.previousElementSibling;
+    }
    
     // Get the name from the modal
     const name = modal.lastElementChild.firstElementChild.nextElementSibling.textContent;
@@ -197,10 +206,10 @@ function toggleEmployee(event) {
     // Get the adjacent employee card
     let adjEmployee;
     let index;
-    if (event.target.id === 'modal-prev') {
+    if (target.className === 'fa fa-chevron-left' || target.id === 'modal-prev') {
         adjEmployee = currentEmployee[0].previousElementSibling;
         index = employeeList.length - 1;
-    } else if (event.target.id === 'modal-next') {
+    } else if (target.className === 'fa fa-chevron-right' || target.id === 'modal-next') {
         adjEmployee = currentEmployee[0].nextElementSibling;
         index = 0;
     }
@@ -223,14 +232,15 @@ gallery.addEventListener('click', function(event) {
 // When the modal close button or overlay is clicked
 body.addEventListener('click', function(event) {
     const target = event.target;
-    if (target.textContent === 'X' || target.className === 'modal-close-btn' || target.className === 'modal-container') {
+    if (target.className === 'fa fa-times' || target.className === 'modal-close-btn' || target.className === 'modal-container') {
         closeModal();
     }
 });
 
 // When the previous or next button is clicked
 body.addEventListener('click', function(event) {
-    if (event.target.id === 'modal-prev' || event.target.id === 'modal-next') {
+    const target = event.target;
+    if (target.className === 'fa fa-chevron-right' || target.className === 'fa fa-chevron-left' || target.id === 'modal-prev' || target.id === 'modal-next') {
         toggleEmployee(event);
     }
 });
